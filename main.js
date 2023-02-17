affb7ac51fac7009ba224476a3ac0ab60686ecdf
Igor Paluch <hello.igor@wp.pl>
2023-02-17 13:56:27 +0100

    Second commit, css swapped to scss

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
-import style from './style.css';
-import { app } from './app';
-
+import "./style.scss";
+import { app } from "./app";
 
 app();
 
 const words = ["computer it", "hacking", "working it", "programming"];
 
 let customBackground = words[Math.floor(Math.random() * words.length)];
-
 
 // function valueInputFunction(valueOfInput) {
 //   console.log(valueOfInput)
@@ -18,35 +16,36 @@ let github = {
   fetchUser: function (userName) {
     userName.split("").join("");
     console.log(userName);
-    fetch("https://api.github.com/users/"+userName)
+    fetch("https://api.github.com/users/" + userName)
       .then((response) => {
-          console.log(response);
-          if (!response.ok) {
-            alert("Uzytkownik nie zostal znaleziony, sprobuj ponownie!");
-            throw new Error("User not found");
-          }
-          return response.json();
-        })
+        console.log(response);
+        if (!response.ok) {
+          alert("Uzytkownik nie zostal znaleziony, sprobuj ponownie!");
+          throw new Error("User not found");
+        }
+        return response.json();
+      })
       .then((data) => this.displayUser(data));
   },
   displayUser: function (data) {
-
     // const { company } = data.company;
     // const { email } = data.email;
-    
+
     // document.querySelector(".city").innerText = "Profile" + login;
     document.querySelector(".profile").innerHTML =
       "Profile name: " + data.login;
     document.querySelector(".id").innerHTML = "id: " + data.id;
     document.querySelector(".avatar").src = data.avatar_url;
-    document.querySelector(".profile-type").innerHTML = "Profile type: " + data.type;
-    document.querySelector(".full-name").innerHTML =
-      "Fullname: " + data.name;
+    document.querySelector(".profile-type").innerHTML =
+      "Profile type: " + data.type;
+    document.querySelector(".full-name").innerHTML = "Fullname: " + data.name;
     document.querySelector(".email").innerHTML = "Email: " + data.email;
-    document.querySelector(".followers").innerHTML = "Followers: " + data.followers;
+    document.querySelector(".followers").innerHTML =
+      "Followers: " + data.followers;
     document.querySelector(".createdAT").innerHTML =
       "Profile created: " + data.created_at;
-    document.querySelector(".repositories").innerHTML = "Repositories: " + data.public_repos;
+    document.querySelector(".repositories").innerHTML =
+      "Repositories: " + data.public_repos;
     document.body.style.backgroundImage =
       "url('https://source.unsplash.com/1600x900/?" + customBackground + "')";
     console.log(customBackground);
@@ -54,11 +53,8 @@ let github = {
   },
   search: function () {
     this.fetchUser(document.querySelector(".search-bar").value);
-    
   },
 };
-
-
 
 document
   .querySelector(".search button")
